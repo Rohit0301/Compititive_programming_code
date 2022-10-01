483ed6f8921e66550da74ed912b2480778367d9f
Siddhartha Katiyar <siddharthakatiyar25@gmail.com>
2022-10-01 21:00:23 +0530

    Added cpp solution for Numbers with Same Consecutive Differences
    Resolves Add leetcode problem solution #3

---
+++ Leetcode/numbers_with_same_consecutive_differences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    vector<int> numsSameConsecDiff(int n, int k) {
+	    for(int i = 1; i < 10; i++)
+		    genNum(n - 1, k, i, i);
+	    return ans;
+    }
+
+    void genNum(int n, int& k, int sum, int last) {
+	    if(n == 0) {
+		    ans.push_back(sum);
+		    return;
+	    }
+	    for(int i = 0; i < 10; i++) {
+		    if(abs(i - last) == k) {
+			    genNum(n - 1, k, sum * 10 + i, i);
+		    }
+	    }
+    }
+};
